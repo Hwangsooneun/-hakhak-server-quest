bb78dc0045dc27a932e995208538b63904027975
Hwangsooneun <catalyst8849@gmail.com>
2021-10-29 17:13:42 +0900

    feat: local test env

---
+++ src/graphql.ts
@@ -0,0 +1,72 @@
+
+/** ------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export interface GetBoardsData {
+    title?: string;
+    content?: string;
+    author?: string;
+}
+
+export interface CreateBoardData {
+    title: string;
+    content: string;
+}
+
+export interface UpdateBoardData {
+    id: number;
+    title?: string;
+    content?: string;
+}
+
+export interface DeleteBoardData {
+    id: number;
+}
+
+export interface CreateUserData {
+    name: string;
+    password: string;
+}
+
+export interface LoginUserData {
+    name: string;
+    password: string;
+}
+
+export interface Board {
+    id: string;
+    createdAt: number;
+    updatedAt: number;
+    deletedAt?: number;
+    title: string;
+    content: string;
+    author: User;
+}
+
+export interface IQuery {
+    getBoards(data?: GetBoardsData): Board[] | Promise<Board[]>;
+    hello(data?: string): string | Promise<string>;
+}
+
+export interface IMutation {
+    createBoard(data?: CreateBoardData): Board | Promise<Board>;
+    updateBoard(data?: UpdateBoardData): Board | Promise<Board>;
+    deleteBoard(data?: DeleteBoardData): boolean | Promise<boolean>;
+    createUser(data: CreateUserData): User | Promise<User>;
+    deleteUser(): boolean | Promise<boolean>;
+    loginUser(data: LoginUserData): string | Promise<string>;
+}
+
+export interface User {
+    id: string;
+    createdAt: number;
+    updatedAt: number;
+    deletedAt?: number;
+    name: string;
+    password: string;
+    boards: Board[];
+}
