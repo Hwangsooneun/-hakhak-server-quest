c5f1c30ea9f80518ee8339084e0a59445e97cd59
Hwangsooneun <catalyst8849@gmail.com>
2021-10-31 12:54:27 +0900

    feat: createBoard and logging query setting

--- src/board/board.resolver.ts
+++ src/board/board.resolver.ts
@@ -1,4 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { GqlAuthGuard } from 'src/auth/jwt-auth.guard';
+import { CurrentUser } from 'src/common/decorators/user.decorator';
+import { UserPayloadDto } from 'src/common/dto/user.payload.dto';
+import { CreateBoardData } from '../graphql';
+import { BoardService } from './board.service';
 
 @Resolver()
-export class BoardResolver {}
+export class BoardResolver {
+  constructor(
+    private boardService: BoardService
+  ) {}
+
+  @Mutation()
+  @UseGuards(GqlAuthGuard)
+  createBoard(
+    @Args ('data') data: CreateBoardData,
+    @CurrentUser() user: UserPayloadDto
+    ) {
+      return this.boardService.createBoard(data.title, data.content, user.id)
+  }
+}
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/user/user.model';
+import { User } from '../user/user.model';
 import { Repository } from 'typeorm';
 import { Board } from './board.model';
 
@@ -9,8 +9,20 @@ export class BoardService {
   constructor(
     @InjectRepository(Board)
     private _boardRepository: Repository<Board>,
+    @InjectRepository(User)
     private readonly _userRepository: Repository<User>
-    ) {
-    console.log('use this repository board', Board);
-  }
+    ) {}
+  
+    public async createBoard(title: string, content: string, userId: number) {
+      const user = await this._userRepository.findOne({
+        where: { id: userId }
+      })
+
+      const board = new Board()
+      board.title = title
+      board.content = content
+      board.author = user
+
+      return this._boardRepository.save(board)
+    }
 }
