218f19cadd1d8093df616071e52405fe4568aff7
Hwangsooneun <catalyst8849@gmail.com>
2021-10-31 12:32:40 +0900

    board branch init

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.model';
 import { Repository } from 'typeorm';
 import { Board } from './board.model';
 
 @Injectable()
 export class BoardService {
-  constructor(@InjectRepository(Board) _boardRepository: Repository<Board>) {
+  constructor(
+    @InjectRepository(Board)
+    private _boardRepository: Repository<Board>,
+    private readonly _userRepository: Repository<User>
+    ) {
     console.log('use this repository board', Board);
   }
 }
